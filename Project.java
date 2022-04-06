467d98162280cbb40f32759b386b387cb8c68f7e
chaitan24 <102653898+chaitan24@users.noreply.github.com>
2022-04-06 11:50:52 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Project
+{
+public static void main(String args[])
+{
+Scanner in =new Scanner(System.in);
+System.out.print("welcome");
+System.out.print("CREAT ACCOUNT");
+System.out.print("ENTER YOUR EMAIL ADDRESS");
+int email=in.nextInt();
+System.out.print("ENTER YOUR EMAIL ADDRESS");
+int email=in.nextInt();
+System.out.print("ENTER YOUR EMAIL ADDRESS");
+int email=in.nextInt();
+System.out.print("ENTER YOUR EMAIL ADDRESS");
+int email=in.nextInt();
+}
+
+
+
+
